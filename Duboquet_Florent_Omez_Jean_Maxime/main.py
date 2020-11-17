af5c4d9e7054176a1279c6115c0b4df276678b74
JeanMax <181727@student.umons.ac.be>
2020-11-17 22:29:57 +0100

    Commit

--- Duboquet_Florent_Omez_Jean_Maxime/main.py
+++ Duboquet_Florent_Omez_Jean_Maxime/main.py
@@ -1,13 +1,23 @@
 from utils import random_select_utterances,plot_signal_and_energy_per_frame
 
-'''
-#Voiced an unvoised threshold
 
+#Energy of the voiced and unvoiced sounds
+'''
 utterances=random_select_utterances(['samples/man','samples/woman'],5)
 for utterance in utterances:
         plot_signal_and_energy_per_frame(utterance,90 / 1000, 90 / 1000)
 '''
 
+#Building a rule-based system
+
 utterances=random_select_utterances(['samples/man','samples/woman'],15)
+
+sexes=[]
+for i in range(len(utterances)):
+    if 'woman' in utterances[i]:
+        sexes.append('woman')
+    else:
+        sexes.append('man')
+
 
 
