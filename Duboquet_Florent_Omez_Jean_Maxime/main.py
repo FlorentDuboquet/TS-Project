5c44d6737d11885104f0c8edaec5778d14c5b142
JeanMax <181727@student.umons.ac.be>
2020-11-17 22:16:07 +0100

    Commit

--- Duboquet_Florent_Omez_Jean_Maxime/main.py
+++ Duboquet_Florent_Omez_Jean_Maxime/main.py
@@ -1,6 +1,8 @@
 from utils import random_select_utterances,plot_signal_and_energy_per_frame
 
 '''
+#Voiced an unvoised threshold
+
 utterances=random_select_utterances(['samples/man','samples/woman'],5)
 for utterance in utterances:
         plot_signal_and_energy_per_frame(utterance,90 / 1000, 90 / 1000)
