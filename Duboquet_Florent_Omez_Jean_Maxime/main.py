3dedfb39e5cffd74a86ad68f7f646fbdd4bd9f45
JeanMax <181727@student.umons.ac.be>
2020-11-18 16:38:34 +0100

    Commit

--- Duboquet_Florent_Omez_Jean_Maxime/main.py
+++ Duboquet_Florent_Omez_Jean_Maxime/main.py
@@ -5,11 +5,11 @@ from os import listdir
 from utils import random_select_utterances,plot_signal_and_energy_per_frame,energy,pitch_autocorrelation,feature_extraction,rule_based_system_on_energy_accurancy,rule_based_system_on_fundamental_frequency_accurancy
 
 print('\n--- Energy of the voiced and unvoiced sounds')
-'''
+
 utterances=random_select_utterances(['samples/man','samples/woman'],5)
 for utterance in utterances:
         plot_signal_and_energy_per_frame(utterance,90 / 1000, 90 / 1000)
-'''
+
 print('\n--- Building a rule-based system')
 
 frame_width=90/1000
@@ -26,7 +26,7 @@ train_data_frame=feature_extraction(train_files_adresse,frame_width,shift_width,
 
 print('\n- Train data frame :\n',train_data_frame)
 
-sns.pairplot(train_data_frame, hue='Sexe')
+sns.pairplot(train_data_frame, hue='Sex')
 plt.show()
 
 sns.heatmap(train_data_frame.corr())
@@ -49,7 +49,7 @@ for threshold_on_fundamental_frequency in range(0,1000,1):
         best_accurancy_on_fundamental_frequency=accurancy
         best_threshold_on_fundamental_frequency = threshold_on_fundamental_frequency
 print('\n- Fundamental frequency :','\nBest threshold :',best_threshold_on_fundamental_frequency,'\nBest accurancy :',best_accurancy_on_fundamental_frequency)
-'''
+
 print('\n-- TEST :')
 
 test_files_adresse=[]
@@ -68,4 +68,3 @@ print('\n- Energy :','\nAccurancy :',accurancy_on_energy)
 
 accurancy_on_fundamental_frequency=rule_based_system_on_fundamental_frequency_accurancy(test_data_frame,best_threshold_on_fundamental_frequency)
 print('\n- Fundamental frequency :','\nAccurancy :',accurancy_on_fundamental_frequency)
-'''
